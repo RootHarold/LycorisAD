bc06465d35bc15da4722f3a6ae92ed1aadea3422
rootharold <rootharold@163.com>
2020-01-19 23:08:22 +0800

    building

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='LycorisAD',
+    version='1.0.0',
+    description="An elegant outlier detection algorithm framework based on AutoEncoder.",
+    author="RootHarold",
+    author_email="rootharold@163.com",
+    url="https://github.com/RootHarold/LycorisAD",
+    py_modules=['LycorisAD'],
+    zip_safe=False,
+    install_requires=['LycorisNet>=2.5', 'deap>=1.3', 'scipy>=1.3', 'numpy>=1.18']
+)
